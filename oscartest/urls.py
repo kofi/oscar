23596d950b99dd14be9e4b580a33819ddb92de7a
kofi <moloo.web@gmail.com>
2013-07-14 15:47:25 -0400

    committing code

---
+++ oscartest/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from oscar.app import application
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'oscartest.views.home', name='home'),
+    # url(r'^oscartest/', include('oscartest.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    
+    # add Oscar shop urls
+    (r'', include(application.urls))
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
